5a0d1495ebc2f6296900679181a27f2ee4672371
Azizbek1 <azizochilov569@gmail.com>
2023-01-15 22:18:20 +0500

    BUG FIX
    `
    ``

--- client/src/actions/file.js
+++ client/src/actions/file.js
@@ -3,12 +3,13 @@ import axios from 'axios'
 export function getFiles(dirId) {
     return async dispatch => {
         try {
-            const response = await axios.get(`localhost:5000/api/files${dirId ? '?parent='+dirId : ''}`, {
+            const response = await axios.get(`http://localhost:5000/api/files${dirId ? '?parent='+dirId : ''}`, {
                 headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}
             });
             console.log(response.data);
         } catch (e) {
-            alert(e.response.data.message)
+            // alert(e.response.data.message)
+            console.log(e.response);
         }
     }
 }
