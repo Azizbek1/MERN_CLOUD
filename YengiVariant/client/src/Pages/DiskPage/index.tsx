a72199e3ee3a264a1ab4aac0e9ec0ab789bafcbd
Azizbek1 <azizochilov569@gmail.com>
2023-01-22 09:43:14 +0500

    ynargi codlar

--- YengiVariant/client/src/Pages/DiskPage/index.tsx
+++ YengiVariant/client/src/Pages/DiskPage/index.tsx
@@ -4,7 +4,7 @@ import DiskStyled from "./Style";
 export default function DiskPage() {
   return (
     <DiskStyled>
-        <h1 className='text-3xl font-bold underline'>React vs Tanwild css</h1>
+        <h1>React vs Tanwild css</h1>
     </DiskStyled>
   )
 }
